b29d082c01e5443159f1b3c11f912b33e389391b
Rifki Ramadhan <riifkiramadhan2@gmail.com>
2023-06-05 17:43:45 +0700

    Add pagination and loading

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,32 @@
-async function fetchPokemonDetail(url) {
+const fetchPokemonDetail = async (url) => {
   try {
     const response = await axios.get(url);
     const pokemonData = response.data;
 
     const htmlContent = `
-      <div class="content-details">
-        <a class="content-back" href="./index.html">&#8249;</a>
-        <h1 class="content-details-heading">${pokemonData.name}</h1>
-        <img class="content-details-img" src="${
-          pokemonData.sprites.front_default
-        }" alt="${pokemonData.name}"/>
-  
-        <table>
-          <tr>
-              <th>Weight</th>
-              <th>Height</th>
-              <th>Types</th>
-          </tr>
-          <tr>
-              <td>${pokemonData.weight}</td>
-              <td>${pokemonData.height}</td>
-              <td>${pokemonData.types
-                .map((types) => types.type.name)
-                .join(', ')}</td>
-          </tr>
-        </table>
-      <div>
-      `;
+        <div class="content-details">
+          <a class="content-back" href="./index.html">&#8249;</a>
+          <h1 class="content-details-heading">${pokemonData.name}</h1>
+          <img class="content-details-img" src="${
+            pokemonData.sprites.front_default
+          }" alt="${pokemonData.name}"/>
+    
+          <table>
+            <tr>
+                <th>Weight</th>
+                <th>Height</th>
+                <th>Types</th>
+            </tr>
+            <tr>
+                <td>${pokemonData.weight}</td>
+                <td>${pokemonData.height}</td>
+                <td>${pokemonData.types
+                  .map((types) => types.type.name)
+                  .join(', ')}</td>
+            </tr>
+          </table>
+        <div>
+        `;
 
     const detailElement = document.createElement('div');
     detailElement.innerHTML = htmlContent;
@@ -38,38 +38,84 @@ async function fetchPokemonDetail(url) {
   } catch (error) {
     console.log(`Terjadi Error Pada ${error}`);
   }
-}
+};
 
-async function searchData() {
+const fetchData = async () => {
+  const ITEMS_PER_PAGE = 10;
+  let currentPage = 1;
+
   try {
     const searchValue = document
       .getElementById('search-input')
       .value.toLowerCase();
+    const resultElement = document.getElementById('result');
+    const loadingElement = document.getElementById('loading');
+    const notFoundElement = document.getElementById('not-found');
+    const paginationElement = document.getElementById('pagination');
+
     const response = await axios.get('https://pokeapi.co/api/v2/pokemon');
     const dataResult = response.data.results;
-    const resultElement = document.getElementById('result');
 
-    let htmlContent = '<ul>';
+    loadingElement.style.display = 'block';
+    notFoundElement.style.display = 'none';
 
-    dataResult.map((pokemon) => {
-      if (pokemon.name.toLowerCase().includes(searchValue)) {
+    const filteredData = dataResult.filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(searchValue)
+    );
+
+    if (filteredData.length === 0) {
+      notFoundElement.style.display = 'block';
+      resultElement.innerHTML = ''; // Mengosongkan hasil pencarian sebelumnya
+      paginationElement.innerHTML = ''; // Menghapus tombol paginasi
+    } else {
+      const totalPages = Math.ceil(filteredData.length / ITEMS_PER_PAGE);
+      let htmlContent = '<ul>';
+
+      const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+      const endIndex = startIndex + ITEMS_PER_PAGE;
+      const paginatedData = filteredData.slice(startIndex, endIndex);
+
+      paginatedData.forEach((pokemon) => {
         htmlContent += `
-        <li>
-          <a href="#" onclick="fetchPokemonDetail('${pokemon.url}')"><span><img src="./src/assets/img/icon.png" width="15px" /> ${pokemon.name}</span> <span>&#8250;</span></a>
-        </li>`;
+          <li>
+            <a href="#" onclick="fetchPokemonDetail('${pokemon.url}')">
+              <span>
+                <img src="./src/assets/img/icon.png" width="15px" /> ${pokemon.name}
+              </span> 
+              <span>&#8250;</span>
+            </a>
+          </li>`;
+      });
+
+      htmlContent += '</ul>';
+
+      resultElement.innerHTML = htmlContent;
+
+      let paginationHtml = '';
+      for (let i = 1; i <= totalPages; i++) {
+        paginationHtml += `
+          <button onclick="changePage(${i})" ${
+          currentPage === i ? 'class="active"' : ''
+        }>${i}</button>
+        `;
       }
-    });
 
-    htmlContent += '</ul>';
+      paginationElement.innerHTML = paginationHtml;
+    }
 
-    resultElement.innerHTML = htmlContent;
+    loadingElement.style.display = 'none';
   } catch (error) {
     console.log(`Terjadi Error Pada: ${error}`);
   }
-}
+};
+
+const changePage = (pageNumber) => {
+  currentPage = pageNumber;
+  fetchData();
+};
 
 document.addEventListener('DOMContentLoaded', () => {
   const searchInput = document.getElementById('search-input');
-  searchInput.addEventListener('input', searchData);
-  searchData();
+  searchInput.addEventListener('input', fetchData);
+  fetchData();
 });
